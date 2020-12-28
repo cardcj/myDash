6b7612b49fd75c4904444261b69f99ef1d463517
cardcj <cardwell.james@gmail.com>
2020-12-28 01:19:45 +0000

    have added global styles for body styling and base font setting to App.tsx, created Todos.tsx component with basic input and button, changed styling of navigation to go with new color pallette

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,38 @@
 import * as React from 'react';
+import { Global } from '@emotion/react';
+import styled from '@emotion/styled';
 
 import Navigation from './components/Navigation';
+import Todos from './components/Todos';
+
+const Container = styled('div')`
+    width: 80%;
+    text-align: center;
+    margin: 0 auto;
+    z-index: 1;
+    background-color: #FFFFFF;
+`
 
 class App extends React.Component {
     render() {
         return (
             <div>
+                <Global
+                    styles={{
+                        body: {
+                            margin: 0,
+                            padding: 0,
+                            backgroundColor: "#D2D2D2"
+                        },
+                        '*': {
+                            fontFamily: "Montserrat, sans-serif"
+                        }                        
+                    }}
+                />
                 <Navigation />
+                <Container>
+                    <Todos />
+                </Container>
             </div>
         );
     };
