b0c5c4dc3c70a509f800ee8793f635208a29b4f9
cardcj <cardwell.jamesc@gmail.com>
2021-01-24 15:05:07 +0000

    removed index.tsx as not needed, integrated react-testing-library

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import ReactDOM from 'react-dom';
 import { Global } from '@emotion/react';
 import styled from '@emotion/styled';
 
@@ -41,4 +42,4 @@ class App extends React.Component {
     };
 }
     
-export default App;
+ReactDOM.render(<App />, document.getElementById('root'));
